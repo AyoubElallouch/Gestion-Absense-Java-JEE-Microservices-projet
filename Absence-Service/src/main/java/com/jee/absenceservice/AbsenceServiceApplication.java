361ffd7f7c51be5b5ad91a2cebf7fc5f930c1784
AyoubElallouch <elallouchayoub00@gmail.com>
2023-01-31 13:37:28 +0100

    First Commit to insure version's between MS

---
+++ Absence-Service/src/main/java/com/jee/absenceservice/AbsenceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jee.absenceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsenceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbsenceServiceApplication.class, args);
+    }
+
+}
