20a6b553bbf7d2be658f956be0a9404cfed93a90
ElkheirImane <102419335+ElkheirImane@users.noreply.github.com>
2023-02-04 19:55:57 +0100

    commit entity, models and repos

--- cours-service/src/main/java/com/jee/coursservice/CoursServiceApplication.java
+++ cours-service/src/main/java/com/jee/coursservice/CoursServiceApplication.java
@@ -1,13 +1,28 @@
 package com.jee.coursservice;
 
+
+import com.jee.coursservice.repositories.CoursRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
-public class CoursServiceApplication {
+public class CoursServiceApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(CoursServiceApplication.class, args);
     }
+    @Autowired
+    CoursRepository coursRepository;
+    @Override
+    public void run(String... args) throws Exception {
+        for (int i = 1; i<4; i++){
 
+        }
+
+
+    }
 }
