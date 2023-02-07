a3dfcd06378d5a7c403b49b0c99ce52f9793e732
AyoubElallouch <elallouchayoub00@gmail.com>
2023-02-07 16:57:07 +0100

    updating Professors MS controller

--- professor-service/src/main/java/com/jee/professorservice/web/ProfessorsController.java
+++ professor-service/src/main/java/com/jee/professorservice/web/ProfessorsController.java
@@ -5,10 +5,23 @@ import com.jee.professorservice.repositories.ProfessorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.awt.*;
+import java.util.List;
+
 @RestController
 public class ProfessorsController {
     @Autowired
     private ProfessorRepository professorRepository;
+
+    @GetMapping("/professors")
+    public List<Professor> professorList(){
+        return professorRepository.findAll();
+    }
+
+    @GetMapping("professors/{id}")
+    public Professor getprofById(@PathVariable Long id){
+        return professorRepository.findById(id).get();
+    }
 
     @PostMapping("/professors")
     public Professor saveProfessor(@RequestBody Professor professor){
